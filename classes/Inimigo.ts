dcd827a27f14004eba95af217323a130a8b4f628
Laís Becker <141473728+laisbecker@users.noreply.github.com>
2024-11-24 15:29:08 -0300

    feat: adicionar classe inimigo

---
+++ classes/Inimigo.ts
@@ -0,0 +1,12 @@
+import Personagem from "./Personagem";
+
+export default class Inimigo extends Personagem {
+    atacar(personagem: Personagem) {
+        const chanceDeAcerto = Math.random()
+        if (chanceDeAcerto < 0.2) {
+            console.log('Ataque do inimigo falhou')
+        } else {
+            personagem.receberDano(this.causarDano())
+        }
+    }
+}
